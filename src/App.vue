7a0676a597eb75f669c623b07a7d5ce3d15c23ae
CedricJankiewicz <pm14cau@eduvaud.ch>
2026-05-06 14:47:45 +0200

    feat: basic responsivity

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <header class="p-2 shadow">
+  <header class="yp-2 shadow">
     <div class="container">
       <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
         <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0 text h5">
@@ -11,7 +11,7 @@ import { RouterLink, RouterView } from 'vue-router'
           <RouterLink to="/about" class="p-1"><li>Film/Série</li></RouterLink>
         </ul>
 
-        <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3 flex-grow-1 mx-3"  style="max-width: 400px;">
+        <form class="col-11 col-lg-auto mb-3 mb-lg-0 me-lg-3 flex-grow-1 mx-3" style="max-width: 350px;">
           <div class="input-group">
           <span class="input-group-text">
             <i class="bi bi-search fs-5"></i>
