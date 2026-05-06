e8e2d097892b36488b7b0980f3ae115e3b3aa794
CedricJankiewicz <pm14cau@eduvaud.ch>
2026-05-06 14:43:17 +0200

    feat: import of bootstrap and base header

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,35 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  <header class="p-2 shadow">
+    <div class="container">
+      <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+        <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0 text h5">
+          <RouterLink to="/" class="p-1"><li>Accueil</li></RouterLink>
+          <RouterLink to="/about" class="p-1"><li>Film/Série</li></RouterLink>
+        </ul>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+        <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3 flex-grow-1 mx-3"  style="max-width: 400px;">
+          <div class="input-group">
+          <span class="input-group-text">
+            <i class="bi bi-search fs-5"></i>
+          </span>
+            <input
+                type="search"
+                class="form-control"
+                placeholder="Search..."
+                aria-label="Search"
+            >
+          </div>
+        </form>
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+        <div class="text-end">
+          <RouterLink to="/profil" class="bi bi-person fs-2"></RouterLink>
+        </div>
+      </div>
     </div>
   </header>
-
   <RouterView />
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
